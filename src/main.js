5d88da5f2bbd3b1dfc67cfce65a6063fe932b64c
DeveloperJosh <bam0909@outlook.com>
2024-06-21 16:52:49 -0400

    chore: Add on-finished npm dependency for response lifecycle logging

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import router from './routes/gogo.js';
 import dotenv from 'dotenv';
+import onFinished from 'on-finished';
 
 dotenv.config();
 
@@ -9,6 +10,23 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Middleware to log requests and responses
+app.use((req, res, next) => {
+    // Log request details
+    console.log(`[${new Date().toLocaleString()}] ${req.method} ${req.originalUrl}`);
+
+    // Hook into the response lifecycle
+    onFinished(res, (err, res) => {
+        if (err) {
+            console.error('Error during response:', err);
+        } else {
+            console.log(`[${new Date().toLocaleString()}] ${res.statusCode} ${res.statusMessage}`);
+        }
+    });
+
+    next();
+});
 
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to NekoNode API' });
