af37b8954230f202fec88d8f482716f47fc38181
DeveloperJosh <bam0909@outlook.com>
2024-06-22 15:31:34 -0400

    started adding filters

---
+++ src/utils/filter.js
@@ -0,0 +1,9 @@
+// filter for gogo.js
+
+export default function filter(source, filter) {
+    return source.filter(filter);
+}
+
+export function filterDubSub(source, filter) {
+    return source.filter(filter);
+}
