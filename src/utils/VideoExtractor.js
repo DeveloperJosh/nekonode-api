45240998f86151272e2ebf94ea1c86b20e9d77a1
DeveloperJosh <bam0909@outlook.com>
2024-06-24 21:39:49 -0400

    refactor: Remove unused code in VideoExtractor.js

--- src/utils/VideoExtractor.js
+++ src/utils/VideoExtractor.js
@@ -3,10 +3,9 @@ import axios from 'axios';
 
 class VideoExtractor {
   constructor() {
-    this.client = axios.create(); // No proxy setup, just a basic axios instance
+    this.client = axios.create();
   }
 
-  // Abstract method placeholders
   async extract(videoUrl) {
     throw new Error('Method "extract" should be implemented in subclasses');
   }
